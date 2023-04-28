663476ef225acac88ef3399b50db6739bb28a255
marco <marcothemars@gmail.com>
2023-04-28 12:19:55 +0200

    The finalized version of the piza flux plot?

--- pisaTower.py
+++ pisaTower.py
@@ -12,6 +12,8 @@ spring = -11.75 * (math.pi/180)
 equinox = 0 * (math.pi/180)
 latitude = 43.7230136 * math.pi/180
 area = 3456.471540
+loop_step = 'define'
+AMOUNT_OF_NODES = 15
 
 
 def N(u):
@@ -44,9 +46,6 @@ def pisa(s):
     result, error, somehin = (nquad(integrall, [u_boundary, t_boundary], full_output=True))
     print(result)
 
-    number_of_iterations = int((6 + (sunset*12/math.pi)) - (6 + (sunrise*12/math.pi)))
-    print("numL ",number_of_iterations)
-    loop_step = (sunset - sunrise) / 15
 
     final_answer = 0
     total_errors = 0
@@ -77,47 +76,21 @@ def pisa(s):
         #change
         old_time += loop_step
         time += loop_step
-    else:
-        # EDGE CASE OF LAST INTERVAL (LAST TIME ITERATION..SUNSET)
-        integrall = lambda u, t: 56.67 * np.dot(V(s, t), N(u)) - (-0.6761758113 * sin(-u) + 0.9478158778) * np.dot(
-            V(s, t), N(u))
-
-        def u_boundary(t):
-            return [(5 * math.pi) / 2 + t, (3 * math.pi) / 2 + t]
-
-        def t_boundary():
-            return [old_time, sunset]
-
-        result, error, somehin = (nquad(integrall, [u_boundary, t_boundary], full_output=True))
-        print("lastcase",result)
-
-        total_errors += error
-        final_answer += result
-
-        # adding points for the plot. We want our flux to be per area and time to be converted to 24. hour thing.
-        plot_points[0].append(6 + (sunset* 12 / math.pi))  # Converting to normal hours.
-        plot_points[1].append(result / area)
-
-    print(final_answer)
-
 
     return plot_points
 
 def plot_flux(points, season_name, color):
-    plt.plot(points[0], points[1],label="time step 0.1", color=color)
+
+    plt.plot(points[0], points[1],"go-",label="time step 0.1", color=color)
     plt.xlabel("Time of the day [hrs]")
     plt.ylabel("Energy per area [kWh/m^2]")
-    hour_ticks = [points[0][0]]
-    print(int(points[0][-1]), season_name)
-    if season_name == "summer":
-        sunrise = min(points[0])
-        sunset = min(points[0])
-
-    plt.xticks([])
-    plt.yticks([round(value, 3) for value in points[1]])
-    return
+    # print(int(points[0][-1]), season_name)
 
 if __name__ == "__main__":
+    # as our loop step we need to take something in the middle, so the integrated values wouldnt be relative
+    winter_sunrise, winter_sunset = get_sunrise_and_sunset(winter)
+    loop_step = (winter_sunset - winter_sunrise) / AMOUNT_OF_NODES
+
     points = pisa(fall)
     plot_flux(points, "Fall", color="orange")
 
@@ -134,4 +107,10 @@ if __name__ == "__main__":
     plot_flux(points, "spring", color="green")
     plt.legend(['Fall', 'Summer', 'Equator', 'Winter', 'Spring'])
     plt.title("Pisa Tower's energy yield per day throughout seasons")
-    #plt.show()
+    # init grid
+    plt.xticks(list(range(4,20)))
+    plt.grid()
+    plt.ylim(0.011,0.05)
+    plt.xlim(4,20)
+
+    plt.show()
